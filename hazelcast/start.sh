7313b21c78f84c3fac4afb65aa10050f20ecab2b
cacciald <leandro.21.2008@gmail.com>
2014-08-22 01:11:43 -0300

    Final release for version hazelcast 3.2.5

--- hazelcast/start.sh
+++ hazelcast/start.sh
@@ -7,7 +7,7 @@ MAX_HEAP=${MAX_HEAP:=1G}
 GROUP_NAME=${GROUP_NAME=dev}
 GROUP_PASS=${GROUP_PASS=devpass}
 
-sed -i .bak "s/GROUP_NAME/$GROUP_NAME" /opt/hazelcast/hazelcast-conf.xml
-sed -i .bak2 "s/GROUP_PASS/$GROUP_PASS" /opt/hazelcast/hazelcast-conf.xml
+sed -i.bak "s/GROUP_NAME/$GROUP_NAME/" /opt/hazelcast/hazelcast-conf.xml
+sed -i.bak2 "s/GROUP_PASS/$GROUP_PASS/" /opt/hazelcast/hazelcast-conf.xml
 
-java -server -Xms$MIN_HEAP -Xmx$MAX_HEAP -Djava.net.preferIPv4Stack=true -Dhazelcast.config=/opt/hazelcast/hazelcast-conf.xml -cp /opt/hazelcast/hazelcast-3.2.5/lib/hazelcast-3.2.5.jar
+java -Xms$MIN_HEAP -Xmx$MAX_HEAP -Djava.net.preferIPv4Stack=true -Dhazelcast.config=/opt/hazelcast/hazelcast-conf.xml -jar /opt/hazelcast/hazelcast-3.2.5/lib/hazelcast-3.2.5.jar
